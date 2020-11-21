b0889ef75e42444d2718a16ba3739e463b052266
dreese33 <dylanjacobreese@gatech.edu>
2020-11-21 11:33:04 -0500

    Added ability to access compounds by name and cid from the NCBI's dataset

---
+++ data/covid.py
@@ -0,0 +1,3 @@
+from pubchem import find_compound_cid
+
+print(find_compound_cid('glucose'))
---
+++ data/pubchem.py
@@ -0,0 +1,34 @@
+# Uses pubchem api to get NCBI datasets
+import requests
+
+base_url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/'
+
+def find_compound_cid(compound):
+    url_extension = 'compound/name/' + compound + '/cids/TXT'
+    endpoint = base_url + url_extension
+    try:
+        r = requests.get(endpoint)
+        cid = r.text.strip()
+        return cid
+    except Exception as exc:
+        print('An exception occurred in find_compound_cid')
+        return -1
+
+
+def compound_information_cid(cid):
+    url_extension = 'compound/cid/' + cid + '/assaysummary/CSV'
+    endpoint = base_url + url_extension
+    try:
+        r = requests.get(endpoint)
+        return r.text
+    except Exception as exc:
+        print('An exception occurred in search_compound_cid')
+        return -1
+
+
+def compound_information(compound):
+    print('Getting compound cid')
+    cid = find_compound_cid(compound)
+    if cid == -1:
+        return -1
+    return compound_information_cid(cid)
